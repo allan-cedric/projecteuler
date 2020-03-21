80af2a8032b82fc073967fe4706b69ed2f8e2423
allan-cedric <allan.py3000@gmail.com>
2020-03-21 10:47:52 -0300

    Solution of problem 3

--- solutions_C/problem3.c
+++ solutions_C/problem3.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define NUM 600851475143
+
 /*
 	If 'p' is prime then return 1,
 	else return 0.
@@ -8,14 +10,13 @@ int is_prime (unsigned long int p) ;
 
 int main ()
 {
-	unsigned long int p = 600851475143 ;
 	unsigned long int i ;
 	unsigned long int mult = 1 ;
 	unsigned long int largest = 0 ;
 	
-	for (i = 2 ; i <= p; i++)
+	for (i = 2 ; i <= NUM; i++)
 	{
-		if (p % i == 0)
+		if (NUM % i == 0)
 		{
 			if (is_prime(i))
 			{
@@ -23,7 +24,7 @@ int main ()
 				mult *= i ;
 			}
 		}
-		if (mult ==	p)
+		if (mult ==	NUM)
 			break ;	
 	}
 
