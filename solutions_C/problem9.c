3238861c5b908e20e2479dbf4b7578d655a9abd7
allan-cedric <allan.py3000@gmail.com>
2020-03-22 19:19:28 -0300

    Solution of problem 9

---
+++ solutions_C/problem9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+	float a, b, c ;
+	for (a = 3; a < 1000; a++)
+	{
+		for (b = a+1; b < 1000; b++)
+		{
+			c = sqrt (a*a+b*b) ;
+			if ((c == (int) c) && (a+b+c == 1000))
+				break ;
+				
+		}
+		if (a+b+c == 1000)
+			break ;
+	}
+	printf ("Product: %.0f\n",a*b*c) ;
+	
+	return 0 ;
+}
