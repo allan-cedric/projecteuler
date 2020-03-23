232aeace0b20b2c539fea8ea11de458116d4f92a
allan-cedric <allan.py3000@gmail.com>
2020-03-23 19:32:19 -0300

    Solution of problem 3 (New)

--- solutions_C/problem03.c
+++ solutions_C/problem03.c
@@ -2,28 +2,18 @@
 
 #define NUM 600851475143
 
-/*
-	If 'p' is prime then return 1,
-	else return 0.
-*/
-int is_prime (unsigned long int p) ;
-
 int main ()
 {
-	unsigned long int i ;
-	unsigned long int mult = 1 ;
+	unsigned long int i = 2;
 	unsigned long int largest = 0 ;
 	unsigned long int num = NUM ;
 	
-	for (i = 2 ; i <= NUM;)
+	while (num != 1)
 	{
-		if ((num % i == 0) && (is_prime(i)))
+		if (num % i == 0)
 		{
 			num = num/i ;
 			largest = i ;
-			mult *= i ;
-			if (mult == NUM)
-				break ;
 		}
 		else i++ ;
 	}
@@ -31,15 +21,4 @@ int main ()
 	printf ("Largest prime factor: %ld\n",largest) ;	
 
 	return 0 ;
-}
-
-int is_prime (unsigned long int p)
-{
-	unsigned long int i ;
-	for (i = 2; i < p; i++)
-	{
-		if (p % i == 0)
-			return 0 ;
-	}
-	return 1 ;
 }
