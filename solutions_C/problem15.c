105894eca25a636514d7fa036ca1fb82c1706ab6
allan-cedric <allan.py3000@gmail.com>
2020-03-24 12:17:42 -0300

    Solution of problem 15

--- solutions_C/problem15.c
+++ solutions_C/problem15.c
@@ -5,7 +5,7 @@ int main ()
 	const int size = 20 ;
 	unsigned long int routes = 1 ;
 	int i ;
-	for (i = 1;	i <= size; i++)
+	for (i = 1; i <= size; i++)
 	{
 		routes *= size + i ;
 		routes /= i ;
