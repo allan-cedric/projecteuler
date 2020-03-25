137d372ae9e078f1f06de3b4f7f9182ddf955807
allan-cedric <allan.py3000@gmail.com>
2020-03-25 10:00:26 -0300

    Solution of problem 18

---
+++ solutions_C/problem18.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void maximum_total (int mat[15][15], int i, int j, int sum, int *largest) ;
+
+int main ()
+{
+	int mat[15][15] ;
+	int i, j ;
+	char str[10];
+	for (i = 0; i < 15; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			scanf ("%s",str) ;
+			mat[i][j] = atoi (str) ;
+		}
+	}
+	
+	int largest = 0 ;
+	maximum_total(mat,0,0,0,&largest) ;
+	printf ("Maximum total: %i\n",largest) ;
+
+	return 0 ;
+}
+
+void maximum_total (int m[15][15], int i, int j, int sum, int *largest)
+{	
+	if (i < 15)
+	{
+		sum += m[i][j] ;
+		maximum_total (m,i+1,j+1,sum,largest) ;
+		maximum_total (m,i+1,j,sum,largest) ;
+	}
+	else if (sum > *largest)
+		*largest = sum ;
+}
+
