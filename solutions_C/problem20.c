a39c733a73b2ecd3d4a79a8a1770b4f4dbf8b2fd
allan-cedric <allan.py3000@gmail.com>
2020-03-25 13:03:43 -0300

    Solution of problem 20

---
+++ solutions_C/problem20.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int digit (char ch) ; 
+
+int main ()
+{
+	int sum = 0 ;
+	char ch ;
+	/* Input - 100! */
+	ch = getchar () ;
+	while (!feof(stdin))
+	{
+		sum += digit(ch) ;
+		ch = getchar () ;
+	}
+
+	printf ("Sum: %i\n",sum) ;
+
+	return 0 ;
+}
+
+int digit (char ch)
+{
+	char str[2] = {ch,'\0'} ;
+	return (atoi(str)) ;
+}
