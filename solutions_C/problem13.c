fbc1fddbeba5227efdbf8df875aee784cf2e0afc
allan-cedric <allan.py3000@gmail.com>
2020-03-27 10:06:55 -0300

    Solution of problem 13

--- solutions_C/problem13.c
+++ solutions_C/problem13.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /*
 	Return a integer value of a digit
@@ -45,29 +46,7 @@ int main ()
 }
 
 int digit (char ch)
-{	
-	switch (ch)
-	{
-		case '0':
-			return 0 ;
-		case '1':
-			return 1 ;
-		case '2':
-			return 2 ;
-		case '3':
-			return 3 ;
-		case '4':
-			return 4 ;
-		case '5':
-			return 5 ;
-		case '6':
-			return 6 ;
-		case '7':
-			return 7 ;
-		case '8':
-			return 8 ;
-		case '9':
-			return 9 ;
-	}
-	return 0 ;	
+{
+	char s[2] = {ch,'\0'} ;	
+	return (atoi(s)) ;	
 }
